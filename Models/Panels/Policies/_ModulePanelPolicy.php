50e0bf75d8a4b8d4967fc9e97080038d457f1bb7
marco76tv <marco.sottana@gmail.com>
2023-03-10 17:26:41 +0100

    up

---
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Spid\Models\Panels\Policies;
+
+use Modules\Cms\Contracts\PanelContract;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+use Modules\Xot\Contracts\UserContract;
+
+class _ModulePanelPolicy extends XotBasePanelPolicy
+{
+}
