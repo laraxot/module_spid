a8b8b7dc5ff89ac9dddc620d4ec104367e359324
marco76tv <marco76tv@users.noreply.github.com>
2023-03-10 16:27:18 +0000

    Lint

--- Models/Panels/Policies/_ModulePanelPolicy.php
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Spid\Models\Panels\Policies;
 
-use Modules\Cms\Contracts\PanelContract;
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
-use Modules\Xot\Contracts\UserContract;
 
-class _ModulePanelPolicy extends XotBasePanelPolicy
-{
+class _ModulePanelPolicy extends XotBasePanelPolicy {
 }
