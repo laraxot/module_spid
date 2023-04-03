d2db4bf2947c8cd2890bf97446861681e0c24581
marco xot <marco.sottana@gmail.com>
2023-04-03 16:51:55 +0200

    up

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -4,63 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Spid\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Route;
+use Modules\Xot\Providers\XotBaseRouteServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider {
-    /**
-     * The module namespace to assume when generating URLs to actions.
-     *
-     * @var string
-     */
-    protected $moduleNamespace = 'Modules\Spid\Http\Controllers';
+class RouteServiceProvider extends XotBaseRouteServiceProvider {
+    protected string $moduleNamespace = 'Modules\Spid\Http\Controllers';
 
-    /**
-     * Called before routes are registered.
-     *
-     * Register any model bindings or pattern based filters.
-     *
-     * @return void
-     */
-    public function boot() {
-        parent::boot();
-    }
+    protected string $module_dir = __DIR__;
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map() {
-        $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes() {
-        Route::middleware('web')
-            ->namespace($this->moduleNamespace)
-            ->group(module_path('Spid', '/Routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes() {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->moduleNamespace)
-            ->group(module_path('Spid', '/Routes/api.php'));
-    }
+    protected string $module_ns = __NAMESPACE__;
 }
